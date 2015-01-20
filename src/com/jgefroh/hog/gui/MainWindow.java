2914c8af549f32ea8c40776f28b1b5421c5fe504
JGefroh <joseph.gefroh@gmail.com>
2015-01-19 14:18:56 -1000

    Added v0.0.3

--- src/com/jgefroh/hog/gui/MainWindow.java
+++ src/com/jgefroh/hog/gui/MainWindow.java
@@ -53,7 +53,7 @@ public class MainWindow {
 
 
     private void createFrameAndMainPanels() {
-        frame = new JFrame("HOG - Highly Opinionated Generator v0.0.2 by Joseph Gefroh");
+        frame = new JFrame("HOG - Highly Opinionated Generator v0.0.3 by Joseph Gefroh");
         frame.setSize(800, 600);  
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
